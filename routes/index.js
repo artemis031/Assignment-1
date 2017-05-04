2f3503ce058e5f297e6ce571d305bd78e69587d7
artemis031 <jcbasilio1@up.edu.ph>
2017-05-04 16:02:17 +0800

    Initizalize assignment

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+
+router.use('/users', function(req,res,next){
+	if(req.body.uname === "narom" && req.body.psw === "cute"){
+		next();
+	}
+	else{
+		res.redirect('/');
+	}
+});
+
+
+
+
+
+
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
